83a640bfa2d5287d8cd1a316367ecfb23d968e66
Mark Veltzer <mark.veltzer@gmail.com>
2012-02-08 19:05:58 +0200

    some improvement

---
+++ keynotejs/LayoutFull.js
@@ -0,0 +1,33 @@
+/*
+ * This is a layout manager that only has one child and gives it
+ * the full size that it gets.
+ *
+ * 	Mark Veltzer
+ */
+function LayoutFull(options) {
+	checkExact(options,new Set());
+	this.element=undefined;
+	this.doDebug=false;
+}
+LayoutFull.prototype.debug=function() {
+	if(this.doDebug) {
+		$.each(arguments,function(i,msg) {
+			console.log(msg);
+		});
+	}
+}
+LayoutFull.prototype.addElement=function(elem,size) {
+	this.debug('addElement '+elem+','+size);
+	if(this.element!=undefined) {
+		throw 'already have element';
+	}
+	this.element=elem;
+}
+/*
+ * This is a the main function. It receives where the widgets under the control
+ * of this layout manger should be. It should do the rest.
+ */
+LayoutFull.prototype.resize=function(x,y,width,height) {
+	this.element.posAbs4(x,y,width,height);
+}
+LayoutResolver.getInstance().addLayoutManager('ratio',LayoutFull);
--- keynotejs/TemplateTitleBullets.js
+++ keynotejs/TemplateTitleBullets.js
@@ -1,6 +1,7 @@
 /*
  * Layout:
  * d1
+ * 	dcenter
  *
  * d2
  *
@@ -17,47 +18,55 @@ function TemplateTitleBullets(options) {
 
 	// create the structure
 	this.d_top=$('<div/>').addClass('templateTop');
-	this.lr=new LayoutRatio({
+
+	this.layout=new LayoutFull();
+	this.layout.addElement(this.d_top);
+
+	this.ld_top=new LayoutRatio({
 		'ratio':1.33,
 	});
-	this.d_top.data('data',this.lr);
+	this.d_top.data('layout',this.ld_top);
 
-	this.d=$('<div/>');
-	this.l1=new LayoutRelative({
+	this.d=$('<div/>').addClass('templateSlide');
+	this.d_top.append(this.d);
+	this.ld_top.addElement(this.d);
+
+	this.ld=new LayoutRelative({
 		'orientation':'vertical',
 	});
-	this.d.data('layout',this.l1);
-	this.d_top.append(this.d);
-	this.lr.addElement(this.d);
+	this.d.data('layout',this.ld);
 
 	this.d1=$('<div/>').addClass('template1');
 	this.d2=$('<div/>').addClass('template2');
 	this.d3=$('<div/>').addClass('template3');
-	this.d4=$('<div/>').addClass('template4');
-	this.d5=$('<div/>').addClass('template5');
-	this.d6=$('<div/>').addClass('template6');
 	this.d.append(this.d1);
 	this.d.append(this.d2);
 	this.d.append(this.d3);
-	this.d.append(this.d4);
-	this.d.append(this.d5);
-	this.d.append(this.d6);
-	this.l1.addElement(this.d1,0.23);
-	this.l1.addElement(this.d2,0.67);
-	this.l1.addElement(this.d3,0.10);
-	this.l2=new LayoutRelative({
+	this.ld.addElement(this.d1,0.23);
+	this.ld.addElement(this.d2,0.67);
+	this.ld.addElement(this.d3,0.10);
+
+	this.d4=$('<div/>').addClass('template4');
+	this.d5=$('<div/>').addClass('template5');
+	this.d6=$('<div/>').addClass('template6');
+	this.d2.append(this.d4);
+	this.d2.append(this.d5);
+	this.d2.append(this.d6);
+	this.ld2=new LayoutRelative({
 		'orientation':'horizontal',
 	});
-	this.l2.addElement(this.d4,0.08);
-	this.l2.addElement(this.d5,0.84);
-	this.l2.addElement(this.d6,0.08);
-	this.d2.data('layout',this.l2);
+	this.ld2.addElement(this.d4,0.08);
+	this.ld2.addElement(this.d5,0.84);
+	this.ld2.addElement(this.d6,0.08);
+	this.d2.data('layout',this.ld2);
+
 	// connect the flow layout
-	this.l3=new LayoutFlow({ 'lines':10 });
-	this.d5.data('layout',this.l3);
+	this.ld5=new LayoutFlow({ 'lines':10 });
+	this.d5.data('layout',this.ld5);
 	// connect the flow layout
-	this.l4=new LayoutFlow({ 'lines':1 });
-	this.d1.data('layout',this.l4);
+	this.ld1=new LayoutFlow({ 'lines':1 });
+	this.d1.data('layout',this.ld1);
+
 	// handle resizes
 	this.resize();
 	$(window).resize(function() {
@@ -69,15 +78,15 @@ function TemplateTitleBullets(options) {
 TemplateTitleBullets.prototype.addElement=function(role,element) {
 	if(role=='title') {
 		this.d1.append(element);
-		this.l4.addElement(element);
+		this.ld1.addElement(element);
 		this.resize();
 	}
 	if(role=='bullet') {
 		this.d5.append(element);
-		this.l3.addElement(element);
+		this.ld5.addElement(element);
 		this.resize();
 	}
 }
 TemplateTitleBullets.prototype.resize=function() {
-	this.lr.resize(0,0,$(window).width(),$(window).height());
+	this.layout.resize(0,0,$(window).width(),$(window).height());
 }
