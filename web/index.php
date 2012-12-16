d7ffecf8f611b849363b20283a551c33b8ddc000
Mark Veltzer <mark.veltzer@gmail.com>
2012-12-16 05:29:54 +0200

    better web page

--- web/index.php
+++ web/index.php
@@ -1,7 +1,15 @@
 <html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Keynote</title>
+		<link rel="shortcut icon" href="favicon.ico"/>
+		<meta name="Description" content="Keynote is a whole new way of doing presentations"/>
+		<meta name="Keywords" content="Mark Veltzer, veltzer, powerpoint, presentations, keynote, xml, web"/>
+	</head>
 	<body>
+		<h1>Welcome to the keynote web site</h1>
 		<p>
-		This is the web site for keynote
+		This is the web site for Keynote.
 		If you want to see a demo and learn about keynot checkout <a href="../keynotejs/keynote.php?presentation=../presentations/keynote.xml">this</a> presentation.
 		</p>
 		<p>
