f539c6bfa73558405038a9a207c736e7a22ee382
Mark Veltzer <mark.veltzer@gmail.com>
2013-04-24 23:22:41 +0300

    another class moved to prototype

--- jssrc/src/TemplateTitleBullets.js
+++ jssrc/src/TemplateTitleBullets.js
@@ -4,95 +4,113 @@
 /*jsl:import LayoutRatio.js*/
 /*jsl:import LayoutFlow.js*/
 
-/*
- * Layout:
- * d1
- *   dcenter
- *
- * d2
- *
- *    d4           d5         d6
- *
- * d3
- */
-function TemplateTitleBullets(options) {
-  // for closure
-  var object = this;
-  // handle arguments
-  checkHasOnly(options, new Set('id'));
-  this.id = options.id;
 
-  // create the structure
-  this.d_top = jQuery('<div/>').addClass('templateTop');
+/**
+  @class A template with titles and bullets
+  Layout:
+    d1
+      dcenter
+    d2
+      d4 d5 d6
+    d3
+  @author mark.veltzer@gmail.com (Mark Veltzer)
+*/
+var TemplateTitleBullets = Class.create(/** @lends TemplateTitleBullets# */{
+  /**
+    Create a new instance of this class.
+    @param {object} options configuration options.
+    @return {TemplateTitleBullets} A new instance of this class.
+    @author mark.veltzer@gmail.com (Mark Veltzer)
+  */
+  initialize: function(options) {
+    // for closure
+    var object = this;
+    // handle arguments
+    checkHasOnly(options, new Set('id'));
+    this.id = options.id;
 
-  this.layout = new LayoutFull();
-  this.layout.addElement(this.d_top);
+    // create the structure
+    this.d_top = jQuery('<div/>').addClass('templateTop');
 
-  this.ld_top = new LayoutRatio({
-    ratio: 1.33
-  });
-  this.d_top.data('layout', this.ld_top);
+    this.layout = new LayoutFull();
+    this.layout.addElement(this.d_top);
 
-  this.d = jQuery('<div/>').addClass('templateSlide');
-  this.d_top.append(this.d);
-  this.ld_top.addElement(this.d);
+    this.ld_top = new LayoutRatio({
+      ratio: 1.33
+    });
+    this.d_top.data('layout', this.ld_top);
 
-  this.ld = new LayoutRelative({
-    orientation: 'vertical'
-  });
-  this.d.data('layout', this.ld);
+    this.d = jQuery('<div/>').addClass('templateSlide');
+    this.d_top.append(this.d);
+    this.ld_top.addElement(this.d);
 
-  this.d1 = jQuery('<div/>').addClass('template1');
-  this.d2 = jQuery('<div/>').addClass('template2');
-  this.d3 = jQuery('<div/>').addClass('template3');
-  this.d.append(this.d1);
-  this.d.append(this.d2);
-  this.d.append(this.d3);
-  this.ld.addElement(this.d1, 0.23);
-  this.ld.addElement(this.d2, 0.67);
-  this.ld.addElement(this.d3, 0.10);
+    this.ld = new LayoutRelative({
+      orientation: 'vertical'
+    });
+    this.d.data('layout', this.ld);
 
-  this.d4 = jQuery('<div/>').addClass('template4');
-  this.d5 = jQuery('<div/>').addClass('template5');
-  this.d6 = jQuery('<div/>').addClass('template6');
-  this.d2.append(this.d4);
-  this.d2.append(this.d5);
-  this.d2.append(this.d6);
-  this.ld2 = new LayoutRelative({
-    orientation: 'horizontal'
-  });
-  this.ld2.addElement(this.d4, 0.08);
-  this.ld2.addElement(this.d5, 0.84);
-  this.ld2.addElement(this.d6, 0.08);
-  this.d2.data('layout', this.ld2);
+    this.d1 = jQuery('<div/>').addClass('template1');
+    this.d2 = jQuery('<div/>').addClass('template2');
+    this.d3 = jQuery('<div/>').addClass('template3');
+    this.d.append(this.d1);
+    this.d.append(this.d2);
+    this.d.append(this.d3);
+    this.ld.addElement(this.d1, 0.23);
+    this.ld.addElement(this.d2, 0.67);
+    this.ld.addElement(this.d3, 0.10);
 
-  // connect the flow layout
-  this.ld5 = new LayoutFlow({ lines: 10 });
-  this.d5.data('layout', this.ld5);
-  // connect the flow layout
-  this.ld1 = new LayoutFlow({ lines: 1 });
-  this.d1.data('layout', this.ld1);
+    this.d4 = jQuery('<div/>').addClass('template4');
+    this.d5 = jQuery('<div/>').addClass('template5');
+    this.d6 = jQuery('<div/>').addClass('template6');
+    this.d2.append(this.d4);
+    this.d2.append(this.d5);
+    this.d2.append(this.d6);
+    this.ld2 = new LayoutRelative({
+      orientation: 'horizontal'
+    });
+    this.ld2.addElement(this.d4, 0.08);
+    this.ld2.addElement(this.d5, 0.84);
+    this.ld2.addElement(this.d6, 0.08);
+    this.d2.data('layout', this.ld2);
 
-  // handle resizes
-  this.resize();
-  jQuery(window).resize(function() {
-    object.resize();
-  });
-  // append
-  jQuery(this.id).append(this.d_top);
-}
-TemplateTitleBullets.prototype.addElement = function(role, element) {
-  if (role == 'title') {
-    this.d1.append(element);
-    this.ld1.addElement(element);
+    // connect the flow layout
+    this.ld5 = new LayoutFlow({ lines: 10 });
+    this.d5.data('layout', this.ld5);
+    // connect the flow layout
+    this.ld1 = new LayoutFlow({ lines: 1 });
+    this.d1.data('layout', this.ld1);
+
+    // handle resizes
     this.resize();
+    jQuery(window).resize(function() {
+      object.resize();
+    });
+    // append
+    jQuery(this.id).append(this.d_top);
+  },
+  /**
+    Add an title or bullet to this template.
+    @param {String} role the role of the element (title or bullet).
+    @param {Element} element the element to add.
+    @author mark.veltzer@gmail.com (Mark Veltzer)
+  */
+  addElement: function(role, element) {
+    if (role == 'title') {
+      this.d1.append(element);
+      this.ld1.addElement(element);
+      this.resize();
+    }
+    if (role == 'bullet') {
+      this.d5.append(element);
+      this.ld5.addElement(element);
+      this.resize();
+    }
+  },
+  /**
+    Method to handle resizing events for this template.
+    @author mark.veltzer@gmail.com (Mark Veltzer)
+  */
+  resize: function() {
+    this.layout.resize(0, 0, jQuery(window).width(), jQuery(window).height());
   }
-  if (role == 'bullet') {
-    this.d5.append(element);
-    this.ld5.addElement(element);
-    this.resize();
-  }
-};
-TemplateTitleBullets.prototype.resize = function() {
-  this.layout.resize(0, 0, jQuery(window).width(), jQuery(window).height());
-};
+});
