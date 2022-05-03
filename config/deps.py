fca0167b72f4e18742ae12b9bc65ae82efe511f7
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-03 08:38:37 +0300

    modernizing

---
+++ config/deps.py
@@ -0,0 +1,26 @@
+tools=[
+    'jsl',
+    'closure',
+]
+packages=[
+    'libitext5-java',
+    'libitext5-java-doc',
+    'libargs4j-java',
+    'libargs4j-java-doc',
+    # for xmllint command line tool for validating the presentations
+    'libxml2-utils',
+    # aspell tool for spell checking the xml files
+    'aspell',
+    # english dictionary for aspell
+    'aspell-en',
+    # hebrew dictionary for aspell
+    'aspell-he',
+    'closure-linter',
+    # to bring dependencies
+    'ivy',
+    'ivy-doc',
+    # for ant(1)
+    'ant',
+    # for javac(1)
+    'openjdk-8-jdk-headless',
+]
