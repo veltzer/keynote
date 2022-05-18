bee6550bb3e35370eec07310ccfa348e27ab592f
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:01 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "keynote"
-project_long_description = "Slide persentation system"
-project_year_started = 2012
-project_description = """Slide persentation system"""
+description_short = "Slide persentation system"
